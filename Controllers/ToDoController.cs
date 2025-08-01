2f47180dee7a9eedcb960a74222d5075f219761b
Ksenia <ksenia_popova.02@mail.ru>
2025-08-02 01:46:47 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiTest.Models;
+
+namespace WebApiTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDoItems> _items = new List<ToDoItems>
+        {
+            new ToDoItems { Id = 1, Title = "Пример задачи", IsCompleted = false }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ToDoItems>> GetAll() => Ok(_items);
+
+        [HttpGet("{id}")]
+        public ActionResult<ToDoItems> GetById(int id)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            return item == null ? NotFound() : Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ToDoItems> Create(ToDoItems item)
+        {
+            item.Id = _items.Any() ? _items.Max(i => i.Id) + 1 : 1;
+            _items.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, ToDoItems updatedItem)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            if (item == null) return NotFound();
+
+            item.Title = updatedItem.Title;
+            item.IsCompleted = updatedItem.IsCompleted;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _items.FirstOrDefault(i => i.Id == id);
+            if (item == null) return NotFound();
+
+            _items.Remove(item);
+            return NoContent();
+        }
+    }
+}
+
---
+++ Models/ToDoItems.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiTest.Models
+{
+    public class ToDoItems
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
